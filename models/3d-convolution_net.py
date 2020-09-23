f470a19c0e70d4dcd6c7e03941c55b34d6fcaa27
Tony Hauptman <thauptmann@users.noreply.github.com>
2020-09-23 10:53:15 +0200

    Add 3d-convolution_net.py
    3D-conv Net is using the network of the aha presentation. Because we need no complex constructions, it uses a keras sequential model.

---
+++ models/3d-convolution_net.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+
+class ThreeDConvolution(keras.Model):
+    def __init__(self):
+        super(ThreeDConvolution, self).__init__()
+        self.model = keras.Sequential(
+            [
+                keras.layers.Conv3D(32, 3, activation='relu'),
+                keras.layers.MaxPool3D(pool_size=(1,2,2)),
+                keras.layers.Conv3D(32, 3, activation='relu'),
+                keras.layers.MaxPool3D(pool_size=(1, 2, 2)),
+                keras.layers.Conv3D(64, 3, activation='relu'),
+                keras.layers.Conv3D(64, 3, activation='relu'),
+                keras.layers.MaxPool3D(pool_size=(1,2,2)),
+                keras.layers.Conv3D(128, 3, activation='relu'),
+                keras.layers.Conv3D(128, 3,activation='relu'),
+                keras.layers.Conv3D(128, 3,activation='relu'),
+                keras.layers.MaxPool3D(pool_size=(2,2,2)),
+                keras.layers.Conv3D(256,3, activation='relu'),
+                keras.layers.Conv3D(256,3, activation='relu'),
+                keras.layers.Conv3D(256,3, activation='relu'),
+                keras.layers.Flatten(),
+                keras.layers.Dense(2048, activation='relu'),
+                keras.layers.Dense(2048, activation='relu'),
+                keras.layers.Dense(1),
+            ]
+        )
+
+    def call(self, inputs):
+        return self.model(inputs)
+
+
