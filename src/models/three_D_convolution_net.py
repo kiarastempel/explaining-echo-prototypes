a65478d0743070961e0151a9d671ac4256afba4b
Tony Hauptman <thauptmann@users.noreply.github.com>
2020-10-13 16:58:03 +0200

    Add mirrored strategy

--- src/models/three_D_convolution_net.py
+++ src/models/three_D_convolution_net.py
@@ -14,7 +14,7 @@ class ThreeDConvolution_Stanford(keras.Model):
         input_shape = (frames, width, height, channels)
         self.model = keras.Sequential(
             [
-                keras.layers.experimental.preprocessing.Rescaling(scale=1 / std, offset=-mean, input_shape=input_shape),
+                keras.layers.experimental.preprocessing.Rescaling(scale=1 / std, offset=-mean/std, input_shape=input_shape),
                 keras.layers.Conv3D(32, 3, activation='relu'),
                 # data_augmentation,
                 keras.layers.MaxPool3D(pool_size=(1, 2, 2)),
@@ -47,7 +47,7 @@ class ThreeDConvolution(keras.Model):
         input_shape = (frames, width, height, channels)
         self.model = keras.Sequential(
             [
-                keras.layers.experimental.preprocessing.Rescaling(scale=1 / std, offset=-mean),
+                keras.layers.experimental.preprocessing.Rescaling(scale=1 / std, offset=-mean / std),
                 keras.layers.Conv3D(32, 3, activation='relu', input_shape=input_shape),
                 keras.layers.MaxPool3D(pool_size=(1, 2, 2)),
                 keras.layers.Conv3D(32, 3, activation='relu'),
