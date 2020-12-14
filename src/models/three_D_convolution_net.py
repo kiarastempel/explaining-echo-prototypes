c0de9667d4738be67f3d47df3b2700f28e98197b
Tony Hauptman <thauptmann@users.noreply.github.com>
2020-12-14 10:26:29 +0100

    Add custom metric stubs

--- src/models/three_D_convolution_net.py
+++ src/models/three_D_convolution_net.py
@@ -1,39 +1,40 @@
 from tensorflow import keras
 
 
-class ThreeDConvolution_Stanford(keras.Model):
+class ThreeDConvolutionVGGStanford(keras.Model):
     def __init__(self, width, height, frames, channels, mean, std):
-        super(ThreeDConvolution_Stanford, self).__init__()
+        super(ThreeDConvolutionVGGStanford, self).__init__()
 
         input_shape = (frames, width, height, channels)
         self.model = keras.Sequential(
             [
                 keras.layers.experimental.preprocessing.Rescaling(scale=1 / std, offset=-mean/std, input_shape=input_shape),
                 keras.layers.Conv3D(32, 3, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.MaxPool3D(pool_size=(1, 2, 2)),
                 keras.layers.Conv3D(32, 3, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.MaxPool3D(pool_size=(1, 2, 2)),
                 keras.layers.Conv3D(64, 3, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.Conv3D(64, 3, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.MaxPool3D(pool_size=(1, 2, 2)),
                 keras.layers.Conv3D(128, 3, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.Conv3D(128, 3, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.Conv3D(128, 3, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.MaxPool3D(pool_size=(2, 1, 1)),
                 keras.layers.Conv3D(256, 3, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.Conv3D(256, 3, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.Flatten(),
+
                 keras.layers.Dense(2048, activation='relu'),
-                # keras.layers.BatchNormalization(),
+                keras.layers.BatchNormalization(),
                 keras.layers.Dense(2048, activation='relu'),
                 keras.layers.Dense(1),
             ]
@@ -41,6 +42,17 @@ class ThreeDConvolution_Stanford(keras.Model):
 
     def call(self, inputs, **kwargs):
         return self.model(inputs)
+
+
+class ThreeDConvolutionResNetStanford(keras.Model):
+    def __init__(self, width, height, frames, channels, mean, std):
+        super(ThreeDConvolutionResNetStanford, self).__init__()
+        input_shape = (frames, width, height, channels)
+
+    def call(self, inputs, **kwargs):
+        return self.model(inputs)
+
+
 
 
 class ThreeDConvolution(keras.Model):
---
+++ src/utils/metrics.py
@@ -0,0 +1,10 @@
+def first_frames():
+    return 0
+
+
+def distinct_mean():
+    pass
+
+
+def overlapping_mean():
+    pass
