f5a705fad5eee3b2d5c5d234c8eb572edc046fb2
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-01-14 14:11:42 +0100

    Change baseline calculation to use train and val split

--- src/utils/calculate_baseline.py
+++ src/utils/calculate_baseline.py
@@ -5,9 +5,11 @@ from pathlib import Path
 
 def calculate_baseline(path, value):
     file_path = Path(path)
-    csv = pd.read_csv(file_path, usecols=(value, ))
-    mean = csv.mean()
-    values_without_mean = (csv - mean).abs()
+    csv = pd.read_csv(file_path, usecols=(value, 'Split'))
+    train_set = csv[csv['Split'] == 'TRAIN']
+    validation_set = csv[csv['Split'] == 'VAL']
+    mean = train_set.mean()
+    values_without_mean = (validation_set - mean).abs()
     baseline = values_without_mean.sum() / len(values_without_mean)
     print("Mean value of", value, "is :", mean.iloc[0])
     print("The baseline of", value, "is:", baseline.iloc[0])
