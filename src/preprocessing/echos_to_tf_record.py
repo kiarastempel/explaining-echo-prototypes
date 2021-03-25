848cf1b18d772350fd2551fecc62cb51ec15395c
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-03-25 12:41:05 +0100

    Check frame not None instead of frame.shape

--- src/preprocessing/echos_to_tf_record.py
+++ src/preprocessing/echos_to_tf_record.py
@@ -49,7 +49,7 @@ def load_video(file_name, needed_frames):
         ret, frame = video.read()
         if frame is not None and frame.shape[2] > 1:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_list.append(cv2.imencode('.jpeg', frame)[1].tostring())
+            frame_list.append(cv2.imencode('.jpeg', frame)[1].tostring())
     video.release()
     return frame_list
 
