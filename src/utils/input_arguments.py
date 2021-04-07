7b7f41e4d7e251a3a93c3bf664682af94eef9637
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-04-07 10:32:45 +0200

    Change standard network to resnet_18

--- src/utils/input_arguments.py
+++ src/utils/input_arguments.py
@@ -13,8 +13,9 @@ def get_train_arguments():
     parser.add_argument('--dataset', default='stanford', choices=['stanford', 'mainz'])
     parser.add_argument('-t', '--target', default='ejection_fraction', choices=['ejection_fraction', 'e_e_prime', 'gls',
                                                                                 'quality', 'all'])
-    parser.add_argument('-m', '--model_name', default='vgg', choices=['vgg', 'resnet_18', 'resnet_34',
-                                                                      'resnet_50', 'se-resnet_18', 'se-resnet_34'])
+    parser.add_argument('-m', '--model_name', default='resnet_18', choices=['vgg', 'resnet_18', 'resnet_34',
+                                                                            'resnet_50', 'se-resnet_18',
+                                                                            'se-resnet_34'])
     parser.add_argument('-n', '--experiment_name', required=True)
     parser.add_argument('--resolution', nargs='+', metavar=('height', 'width'), type=int)
 
