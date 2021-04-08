48c6ce94f16bc1cd9d8c72409e8e060f347bcb93
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-04-08 22:58:00 +0200

    Adapt line to max values

--- src/utils/visualise.py
+++ src/utils/visualise.py
@@ -2,12 +2,13 @@ import io
 import seaborn as sns
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import numpy as np
 
 
 def create_scatter_plot(x, y, target, dataset):
     sns.set_style('darkgrid')
     sns.scatterplot(x=x, y=y)
-    plt.plot([0, 100], [0, 100], 'black', lw=1)
+    plt.plot([0, np.max(x)], [0, np.max(y)], 'black', lw=1)
     plt.xlabel('Manual measured values')
     plt.ylabel('Automatic predicted values')
     plt.title(f'Comparison of {target} on {dataset} data')
