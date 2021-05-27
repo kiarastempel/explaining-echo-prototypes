af4b9989cd864e2d198ec5107451d4ae0d40c109
kstempel <kstempel@students.uni-mainz.de>
2021-05-27 15:08:09 +0200

    Calculate axis lengths of frames in order to assign correct volumes

--- src/explainability_two_d/two_D_preprocessing.py
+++ src/explainability_two_d/two_D_preprocessing.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from PIL import Image
+from math import sqrt
 import cv2
 import pandas as pd
 import tensorflow as tf
@@ -65,37 +66,67 @@ def create_still_images(volume_tracings_data_frame, avi_directory, output_direct
 def get_volume_tracings(metadata_path, volumes_path, output_directory):
     file_list_data_frame = pd.read_csv(metadata_path, sep=',', decimal='.')[['FileName', 'ESV', 'EDV']]
     file_list_data_frame['FileName'] = file_list_data_frame['FileName'].astype(str) + '.avi'
-    volume_tracings_data_frame = pd.read_csv(volumes_path, sep=',', decimal='.')[['FileName', 'Frame']]
+    volume_tracings_data_frame = pd.read_csv(volumes_path, sep=',', decimal='.')
+    volume_tracings_data_frame = calc_axis_length(volume_tracings_data_frame)[['FileName', 'Frame', 'AxisLength']]
+    # volume_tracings_data_frame['FileName'] = volume_tracings_data_frame['FileName'].astype(str) + '.avi'
     volume_tracings_data_frame = volume_tracings_data_frame.groupby(
         ['FileName', 'Frame']).head(1).reset_index(drop=True)
     esv_tracings_data_frame = volume_tracings_data_frame.loc[
-        volume_tracings_data_frame.groupby('FileName')['Frame'].idxmin()] \
+        volume_tracings_data_frame.groupby('FileName')['AxisLength'].idxmin()] \
         .merge(file_list_data_frame[['FileName', 'ESV']], on='FileName',
                how='inner') \
         .rename(columns={'ESV': 'Volume'})
     edv_tracings_data_frame = volume_tracings_data_frame.loc[
-        volume_tracings_data_frame.groupby('FileName')['Frame'].idxmax()] \
+        volume_tracings_data_frame.groupby('FileName')['AxisLength'].idxmax()] \
         .merge(file_list_data_frame[['FileName', 'EDV']], on='FileName',
                how='inner') \
         .rename(columns={'EDV': 'Volume'})
     volume_tracings_data_frame = pd.concat(
         [esv_tracings_data_frame, edv_tracings_data_frame]).reset_index(drop=True)
+    volume_tracings_data_frame = volume_tracings_data_frame.sort_values(
+        by=['FileName', 'Frame']).reset_index(drop=True)
     volume_tracings_data_frame['Image_FileName'] = \
         volume_tracings_data_frame['FileName'].str.replace(r'.avi$', '') + '_' \
         + volume_tracings_data_frame['Frame'].astype(str) + '.png'
+    print(volume_tracings_data_frame[['FileName', 'Frame', 'AxisLength', 'Volume']])
 
     # split into train, validation and test data
     volume_tracings_data_frame['Split'] = ''
+    volume_tracings_data_frame = volume_tracings_data_frame.sort_values(by=['FileName', 'Frame']).reset_index(drop=True)
+    counter_smaller = 0
+    counter_bigger = 0
     for i, row in volume_tracings_data_frame.T.iteritems():
-        r = random.uniform(0, 1)
-        if r < 0.75:  # 75% train
-            volume_tracings_data_frame.at[i, 'Split'] = 'TRAIN'
-        elif r < 0.875:  # 12.5% test
-            volume_tracings_data_frame.at[i, 'Split'] = 'TEST'
-        else:  # 12.5% validation
-            volume_tracings_data_frame.at[i, 'Split'] = 'VAL'
+        # assign both frames of same video to same dataset
+        if i % 2 == 0:
+            if volume_tracings_data_frame.at[i, 'AxisLength'] < volume_tracings_data_frame.at[i + 1, 'AxisLength']:
+                counter_smaller += 1
+            else:
+                counter_bigger += 1
+            if (volume_tracings_data_frame.at[i, 'AxisLength'] < volume_tracings_data_frame.at[i + 1, 'AxisLength']
+                    and volume_tracings_data_frame.at[i, 'Volume'] > volume_tracings_data_frame.at[i + 1, 'Volume']
+                    or volume_tracings_data_frame.at[i, 'AxisLength'] > volume_tracings_data_frame.at[i + 1, 'AxisLength']
+                    and volume_tracings_data_frame.at[i, 'Volume'] < volume_tracings_data_frame.at[i + 1, 'Volume']):
+                print('Wrong assigment at index', i)
+            r = random.uniform(0, 1)
+            if r < 0.75:  # 75% train
+                volume_tracings_data_frame.at[i, 'Split'] = 'TRAIN'
+                volume_tracings_data_frame.at[i + 1, 'Split'] = 'TRAIN'
+            elif r < 0.875:  # 12.5% test
+                volume_tracings_data_frame.at[i, 'Split'] = 'TEST'
+                volume_tracings_data_frame.at[i + 1, 'Split'] = 'TEST'
+            else:  # 12.5% validation
+                volume_tracings_data_frame.at[i, 'Split'] = 'VAL'
+                volume_tracings_data_frame.at[i + 1, 'Split'] = 'VAL'
     volume_tracings_data_frame.to_csv(Path(output_directory, 'FrameVolumes.csv'), index=False)
+    print('smaller', counter_smaller)
+    print('bigger', counter_bigger)
     return volume_tracings_data_frame
+
+
+def calc_axis_length(df):
+    df['AxisLength'] = ((df.X1.sub(df.X2) ** 2).add(
+        df.Y1.sub(df.Y2) ** 2)) ** 0.5
+    return df
 
 
 if __name__ == '__main__':
